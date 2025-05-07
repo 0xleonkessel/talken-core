8a6a894f2c2a5602fd1eb0cc8e2cdc8833591f8c
leon kessel <leonkesseltalken@gmail.com>
2025-05-07 22:32:00 +0200

    init: add core handler for challenge result processing

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // talken-core entry point
 // todo: implement quiz engine, reward logic, lenny's fury
+
+const { getDailyReward } = require('./rewardSystem');
+const { updateStreak } = require('./streakTracker');
+const { evaluateAnswer } = require('./challengeEngine');
+
+function handleChallengeResult(user, answer, challenge) {
+  const isCorrect = evaluateAnswer(answer, challenge);
+  if (isCorrect) {
+    updateStreak(user);
+    return getDailyReward(user);
+  } else {
+    return { reward: 0, correct: false };
+  }
+}
+
+module.exports = { handleChallengeResult };
