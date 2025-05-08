fcfcbaa0951aacb1dbbf9275b36b0b1baad625d7
leon kessel <leonkesseltalken@gmail.com>
2025-05-08 10:30:40 +0200

    chore: add mock challenge data for evaluation testing

---
+++ challenges.js
@@ -0,0 +1,16 @@
+// mock language challenges
+
+const challenges = [
+  {
+    id: 'ch1',
+    prompt: 'Translate to English: "hola"',
+    solution: 'hello',
+  },
+  {
+    id: 'ch2',
+    prompt: 'Translate to Spanish: "good night"',
+    solution: 'buenas noches',
+  },
+];
+
+module.exports = challenges;
